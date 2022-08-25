6dc3be76f33050723713c12ad58719298b74c1e1
David Daso <daso.timileyin@gmail.com>
2022-08-25 11:47:48 +0100

    first commit

---
+++ structure.py
@@ -0,0 +1,12 @@
+pos =0
+while pos < 5:
+    print(pos, end='')
+    pos +=1
+print(' ')
+
+for pos in range(5):
+    print(pos, end=',')
+print(' ')
+
+for pos in range(5, 10):
+    print(pos, end=',')
