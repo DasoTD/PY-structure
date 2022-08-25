bcba15487a6b9b87ba52815cf9eaca3206838b62
David Daso <daso.timileyin@gmail.com>
2022-08-25 12:31:03 +0100

    function

--- structure.py
+++ structure.py
@@ -13,3 +13,30 @@ for pos in range(5, 10):
 print(' ')
 for pos in range(5, 10, 2): #the third value is called step
     print(pos, end=',')
+print(' ')
+for pos in range(10, 0, -2): #the third value is called step
+    print(pos, end=',')
+print(' ')
+
+
+# n = int(input())
+# for i in range(n):
+#     for j in range(n):
+#         if i ==j or n-i-1 ==j:
+#             print("*", end='')
+#         else:
+#             print("", end="")
+#         print()
+def our_min(a,b):
+    if a<b:
+        return a
+    else:
+        return b
+def our_max(a, b):
+    if a>b:
+        return a
+    else:
+        return b
+a,b = 5,10
+our_min(a,b)
+our_max(a,b)
